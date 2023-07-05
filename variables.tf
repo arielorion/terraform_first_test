2289d243af3564171487344db61050de05d6a276
arielorion <107570114+arielorion@users.noreply.github.com>
2023-07-05 00:38:03 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,3 +16,12 @@ variable "instance_name" {
   default     = "Provisioned by Terraform"
 }
 
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Secret access key for AWS"
+  type        = string
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "Secret access key ID for AWS"
+  type        = string
+}
