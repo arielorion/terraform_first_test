0908713b538539e7e5fc81e003d3dad7e3e4b8e7
arielorion <107570114+arielorion@users.noreply.github.com>
2023-07-05 00:32:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@
 
 provider "aws" {
   region = var.region
+  access_key  = var.AWS_ACCESS_KEY_ID
+  secret_key  = var.AWS_SECRET_ACCESS_KEY
+  account_id  = 311758446456
 }
 
 data "aws_ami" "ubuntu" {
